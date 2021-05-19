0f01ae60a19d03adfef8695b10ac00190a634d65
kingly77 <shelbygj97@gmail.com>
2021-05-18 18:22:52 -0700

    HOTFIX

---
+++ test.js
@@ -0,0 +1,21 @@
+function a(){
+    let t =1;
+
+    function test()
+    {
+        return t;
+    }
+return test
+}
+
+let foo = a();
+
+if( 1 === 'a' )
+
+    let foo = 1;
+foo = 'two';
+foo = {
+    a :1,
+}
+
+console.log(foo());
